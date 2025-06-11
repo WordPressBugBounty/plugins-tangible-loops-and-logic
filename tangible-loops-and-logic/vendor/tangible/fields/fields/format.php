b4a57d26ca793fbfaf67a8ae665e7612a109de30
Sean Murphy <sean@iamseanmurphy.com>
2025-06-11 09:18:24 +0000

    4.2.3

--- tangible-loops-and-logic/vendor/tangible/fields/fields/format.php
+++ tangible-loops-and-logic/vendor/tangible/fields/fields/format.php
@@ -98,6 +98,7 @@ $fields->format_args = function(
       $args = $fields->format_value($args, 'search_url', 'searchUrl');
       $args = $fields->format_value($args, 'ajax_action', 'ajaxAction');
       $args = $fields->format_value($args, 'read_only', 'readOnly');
+      $args = $fields->format_value($args, 'debounce_time', 'debounceTime');
       break;
 
     case 'file':
@@ -113,6 +114,7 @@ $fields->format_args = function(
       $args = $fields->format_value($args, 'use_bulk', 'useBulk');
       $args = $fields->format_value($args, 'section_title', 'sectionTitle');
       $args = $fields->format_value($args, 'header_fields', 'headerFields');
+      $args = $fields->format_value($args, 'new_item', 'newItem');
       break;
 
     case 'field_group':
